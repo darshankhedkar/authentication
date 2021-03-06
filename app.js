d2bb35d99252b87a7b2b63b20f6c5f26b8ec3da0
Darshan khedkar <darshankhedkar.00.dk@gmail.com>
2021-03-06 12:22:00 +0530

    added enviroment var

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 //jshint esversion:6
+require('dotenv').config();
 const express=require("express");
 const bodyParser=require("body-parser");
 const ejs =require("ejs");
 const mongoose=require("mongoose");
+const encrypt=require("mongoose-encryption");
 
 const app=express();
 
@@ -10,12 +12,16 @@ app.use(express.static("public"));
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 
-mongoose.connect("mongodb://localhost:27017/userDB",{useUnifiedTopology:true,useNewUrlParser:true});
+mongoose.connect("mongodb://localhost:27017/userDB",{useUnifiedTopology:true,
+  useNewUrlParser:true});
 
-const userSchema={
+const userSchema=new mongoose.Schema({
   email:String,
   password:String
-};
+});
+
+userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields:["password"]});
+
 
 const User=new mongoose.model("User",userSchema);
 
@@ -32,11 +38,14 @@ app.get("/login",function(req,res){
 app.get("/register",function(req,res){
   res.render("register");
 });
+
+//catch the user from the page
 app.post("/register",function(req,res){
   const newUser=new User({
     email:req.body.username,
     password:req.body.password
   });
+  //save the user and callback
   newUser.save(function(err){
     if(err){
       console.log(err);
@@ -45,9 +54,14 @@ app.post("/register",function(req,res){
     }
   });
 });
+
+
 app.post("/login",function(req,res){
 const username = req.body.username;
 const password = req.body.password;
+
+//this below method is for checking the email
+//and password is same or not which we entered int the register page
 
 User.findOne({email: username},function(err,foundUser){
 if(err){
